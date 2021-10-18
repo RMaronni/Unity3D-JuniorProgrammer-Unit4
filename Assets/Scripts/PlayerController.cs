d43d488a4e59afb9317636bde337bdf8834c631a
Renan Maronni <renanmaronni@gmail.com>
2021-10-18 11:54:22 +0200

    Add an enemy and a physics material.

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    public float speed = 5.0f;
+    public float speed = 3.0f;
     private Rigidbody playerRb;
     private GameObject focalPoint;
 
